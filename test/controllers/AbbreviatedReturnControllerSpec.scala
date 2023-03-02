a27c83eaa1e4ed7c41339b341afb9b0f893c2fde
mywyau <47248104+mywyau@users.noreply.github.com>
2023-03-02 20:43:03 +0000

    DDCE-3527 Upgrade to Scala 2.13.10

--- test/controllers/AbbreviatedReturnControllerSpec.scala
+++ test/controllers/AbbreviatedReturnControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -107,7 +107,7 @@ class AbbreviatedReturnControllerSpec extends AnyWordSpec with Matchers with Gui
       val fakeRequest = FakeRequestWithHeaders.withJsonBody(exampleJsonBody)
       val controller  = new AbbreviatedReturnController(authenticatedAction, Helpers.stubControllerComponents())
       val result      = controller.abbreviation()(fakeRequest)
-      Try((contentAsJson(result) \ "acknowledgementReference").as[UUID]) should be a 'success
+      Try((contentAsJson(result) \ "acknowledgementReference").as[UUID]) should be a Symbol("success")
     }
 
     "returns a 500 when a ServerError agent name is passed" in {
