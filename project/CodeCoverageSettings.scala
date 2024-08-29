0d6828c0ae1474bdd7a682cb23d49fd8432093c3
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2024-08-29 13:28:57 +0100

    edited imports replacing _ with * and coverageExcludedFiles

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -1,10 +1,10 @@
 import sbt.Setting
-import scoverage.ScoverageKeys.*
+import scoverage.ScoverageKeys.{coverageExcludedFiles, coverageFailOnMinimum, coverageHighlighting, coverageMinimumStmtTotal}
 
 object CodeCoverageSettings {
 
   val settings: Seq[Setting[?]] = Seq(
-    coverageExcludedFiles := ".*Routes.*",
+    coverageExcludedFiles := ".*/Routes.*",
     coverageMinimumStmtTotal := 100,
     coverageFailOnMinimum := true,
     coverageHighlighting := true
