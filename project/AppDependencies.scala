f52d18dd73fcea43446b830062657b9e35c8fd57
lmugnai <11977337+lmugnai@users.noreply.github.com>
2026-03-09 15:45:24 +0000

    Update deps + plugins to address security vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "10.7.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "com.networknt"                 % "json-schema-validator"     % "1.5.7",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.19.0"
+    "com.networknt"                 % "json-schema-validator"     % "1.5.9",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.21.1"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
